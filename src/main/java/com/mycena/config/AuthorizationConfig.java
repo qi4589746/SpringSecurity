dd33a12fb4cb86d0fbf9c76591d9722b3ef13b4e
jihung <t256413@gmail.com>
2018-04-03 14:42:24 +0800

    Change token to JWT. The configure(AuthorizationServerEndpointsConfigurer endpoints) in AuthorizationConfig like the configureGlobal(final AuthenticationManagerBuilder auth) in ResourceServerConfig. (Setting where are the user data, encoding way bla bla bla...) The configure(ClientDataServiceConfiguration clients) in AuthorizationConfig like the userDetailsService() in ResourceServerConfig. (Setting user data and some config.)

--- src/main/java/com/mycena/config/AuthorizationConfig.java
+++ src/main/java/com/mycena/config/AuthorizationConfig.java
@@ -1,15 +1,18 @@
 package com.mycena.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * Created by jihung on 4/2/18.
@@ -27,17 +30,30 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 //    @Autowired
 //    private AuthenticationManager authenticationManager;
 
+    @Value("mycena")
+    private String signingKey;
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey(signingKey);
+        return accessTokenConverter;
+    }
+
     @Bean
     public TokenStore tokenStore(){
-        return new InMemoryTokenStore();
+//        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore())
-//                .authenticationManager(authenticationManager)
-                .allowedTokenEndpointRequestMethods(HttpMethod.GET,HttpMethod.POST);
+                .accessTokenConverter(accessTokenConverter())
+                .tokenServices(toPrimarykenServices())
+                .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST)
+        ;
     }
 
     @Override
@@ -46,14 +62,15 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
                 .withClient("client1")
                 .secret("client1")
                 .authorities("ROLE_ADMIN")
-                .authorizedGrantTypes("client_credentials")
+                .authorizedGrantTypes("client_credentials", "password", "refresh_token")
                 .scopes("app1")
                 .autoApprove("app1")
-                .accessTokenValiditySeconds(3600);
+                .accessTokenValiditySeconds(2000)
+                .refreshTokenValiditySeconds(2000);
     }
     //===Can get token by postman tool.===
     //Auth URL: http://localhost:8080/oauth/authorize
-    //Access Token URL: http://localhost:8080/oauth/token
+    //Access Token URL: http://localhost:8080/oauth/token10000
     //Client ID: client1
     //Client Secret: client1
     //Scope(Optional): app1
@@ -65,4 +82,18 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 //                .tokenKeyAccess("hasAuthority('ROLE_ADMIN')")
 //                .checkTokenAccess("hasAuthority('ROLE_ADMIN')");
 //    }
+
+
+    @Bean
+    @Primary
+    public DefaultTokenServices toPrimarykenServices() {
+        DefaultTokenServices services = new DefaultTokenServices();
+        services.setTokenStore(tokenStore());
+        services.setSupportRefreshToken(true);
+        services.setTokenEnhancer(accessTokenConverter());
+        services.setAccessTokenValiditySeconds(accessTokenValiditySeconds);
+        services.setRefreshTokenValiditySeconds(refreshTokenValiditySeconds);
+        return services;
+    }
+
 }
